31879002544e0b07302fe46ad2986fdb64097e59
Igor Barcelos <igor-barcelos@triazur.com>
2025-04-18 16:12:56 +0200

    save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -12,3 +12,6 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
